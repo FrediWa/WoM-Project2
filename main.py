ebda0d97fcc1fdefe31d597c56164724e0ad469b
Fredrik Wasström <fredrik.wasstrom@gmail.com>
2021-10-07 22:43:52 +0300

    Add endpoints for services

--- main.py
+++ main.py
@@ -6,15 +6,19 @@ import os
 load_dotenv()
 # os.environ.get('TEST')
 
-
 app = Flask(__name__)
 api = Api(app)
 
-class HelloWorld(Resource):
+class Services(Resource):
     def get(self):
-        return {'message': "Hello World"}
+        return {'message': "GET"}
+    def post(self):
+        return {'message': "POST"}
+    def patch(self):
+        return {'message': "PATCH"}
+    def delete(self):
+        return {'message': "DELETE"}
         
-
 api.add_resource(HelloWorld, "/helloworld")
 
 
