ec274e36bdeec04dc4d09d339f98d8ac824b60f4
Fredrik Wasström <fredrik.wasstrom@gmail.com>
2021-10-11 23:25:27 +0300

    Logout button deletes jwt cookie

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
 
+const logout = () => {
+  console.log("Log out");
+  document.cookie = "jwt=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+}
+
 function Header() {
   return (
     <header className='header'>
       <h4>🏡 Cabin Services</h4>
       <div class='header__buttons'>
         <a href='https://wom-project-1.herokuapp.com/' target='_blank' class='btn' rel="noreferrer">Rent A Cabin <i class="fas fa-external-link-alt"></i></a>
-        <button class='btn'>Log Out <i class="fas fa-sign-out-alt"></i></button>
+        <button onClick={logout} class='btn'>Log Out <i class="fas fa-sign-out-alt"></i></button>
       </div>
     </header>
   );
