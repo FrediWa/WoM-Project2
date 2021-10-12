1bd70bf20b1609c8e3e795852baaa4170ae87387
SamuelGranvik <samuel.granvik@wikstrommedia.fi>
2021-10-12 16:19:52 +0300

    Updated regex to escape the dots in the JWT - Some minor JSX syntax changes.

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -15,34 +15,40 @@ const requestLogin = async (email, password, loginErr) => {
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify({ 'email': email, password: password })
   }
+
   console.info('Attempting to login')
-  const regex = /.{36}.{126}.{45}/
+
   try {
     const response = await fetch('https://wom-project-1.herokuapp.com/users/login', requestOptions)
     const token = await response.text()
-    if(token.match(regex)){
+    const regex = /.{36}\..{126}\..{43}/
+
+    if (token.match(regex)){
       const d = new Date()
       d.setTime(d.getTime() + 1000*60*60*2)
       
-      document.cookie = 'jwt='+token+';expires='+d.toUTCString();
-      console.info('Login successful');
+      document.cookie = 'jwt='+token+'expires='+d.toUTCString()
+      console.info('Login successful')
       window.location = './dashboard'
+
     } else {
-      console.error('Incorrect password');
+      console.error('Incorrect password')
     }
+
   } catch (error) {
-    console.error(error);
+    console.error(error)
   }
 }
 
 function Login() {
-  const userRef = React.useRef();
-  const passRef = React.useRef();
-  const loginErr = React.useRef();
+  const userRef = React.useRef()
+  const passRef = React.useRef()
+  const loginErr = React.useRef()
   const handleLogin = async (e) => {
     e.preventDefault()
     requestLogin(userRef.current.value, passRef.current.value, loginErr)
   }
+
   return (
     <form className='login-form' onSubmit={handleLogin}>
         <Field ref={userRef} label='Username:' type='text' />
@@ -51,7 +57,7 @@ function Login() {
           <button type='submit' className='btn'>Login</button>
         </div>
       </form>
-  );
+  )
 }
 
-export default Login;
+export default Login
